895a1ab2505064d101e6270d22cad9105bf604da
Reretheplayer <43102929+Reretheplayer@users.noreply.github.com>
2022-01-21 23:46:15 -0500

    move in 8 dir
    read title my guy

--- Creature/Spawner.gd
+++ Creature/Spawner.gd
@@ -2,15 +2,36 @@ extends Node
 # var dec
 var xcord
 var zcord
+var timer = 0
+var timer_limit = 2
+var rng = RandomNumberGenerator.new()
+var scene = load("res://Creature/Creature.tscn")
 func _ready():
 	print("yo")
-	var scene = load("res://Creature/Creature.tscn")
+	
 	# Spawn i amount of creature in genesis gen
-	for i in 20:
+#	for i in 20:
+#		var player = scene.instance()
+#		add_child(player)
+#		xcord = rand_range(0, 48)
+#		zcord = rand_range(0, 48)
+#		player.transform.origin = Vector3(xcord, 15, zcord)
+#		rng.randomize()
+#		var numba = rng.randf_range(0, 10.0)
+#		player.rotation = Vector3(0, numba , 0)
+
+	
+	
+func _physics_process(delta):
+	timer += delta
+	if (timer > timer_limit):
+		
+		timer = 0
 		var player = scene.instance()
 		add_child(player)
 		xcord = rand_range(0, 48)
 		zcord = rand_range(0, 48)
 		player.transform.origin = Vector3(xcord, 15, zcord)
-	
-	
+		rng.randomize()
+		var numba = rng.randf_range(0, 10.0)
+		player.rotation = Vector3(0, numba , 0)
--- Creature/imalive.gd
+++ Creature/imalive.gd
@@ -1,7 +1,8 @@
 extends KinematicBody
-
+var rng = RandomNumberGenerator.new()
 export var gravity := 9.8
 var _velocity := Vector3()
+var numba
 var timer = 0
 var timer_limit = 2
 func _ready():
@@ -9,18 +10,55 @@ func _ready():
 	
 	
 	
-func _process(delta):
+func _physics_process(delta):
+	
 	if not is_on_floor():
 		_velocity.y -= gravity * delta
-	move_and_slide(_velocity, Vector3.UP)
-
+	
+	_velocity = move_and_slide(_velocity, Vector3.UP)
+	
+	
+	
+	
 	timer += delta
+	
 	#Nothing to do, move in a random direction
 	if (timer > timer_limit):
 		
-		timer -= delta
-		var dir = randi() % 1
+		timer = 0
+		rng.randomize()
+		numba = round(rng.randf_range(0, 7))
 		
-		#if dir == 1:
-			#_velocity -= transform.basis.z
+		if numba == 10:
+			_velocity.x = 5
+			_velocity.z = 0
+			self.rotation = Vector3(0, 0 , 0)
+		if numba == 1:
+			_velocity.z = 5
+			_velocity.x = 0
+			self.rotation = Vector3(0, PI * 3/2 , 0)
+		if numba == 2:
+			_velocity.x = -5
+			_velocity.z = 0
+			self.rotation = Vector3(0, PI , 0)
+		if numba == 3:
+			_velocity.z = -5
+			_velocity.x = 0
+			self.rotation = Vector3(0, PI/2 , 0)
+		if numba == 4:
+			_velocity.x = 5 * sqrt(2)/2
+			_velocity.z = 5 * sqrt(2)/2
+			self.rotation = Vector3(0, PI * 7/4 , 0)
+		if numba == 5:
+			_velocity.z = 5 * sqrt(2)/2
+			_velocity.x = -5 * sqrt(2)/2
+			self.rotation = Vector3(0, PI * 5/4 , 0)
+		if numba == 6:
+			_velocity.x = 5 * sqrt(2)/2
+			_velocity.z = -5 * sqrt(2)/2
+			self.rotation = Vector3(0, PI/4 , 0)
+		if numba == 7:
+			_velocity.z = -5 * sqrt(2)/2
+			_velocity.x = -5 * sqrt(2)/2
+			self.rotation = Vector3(0, 3/4 * PI , 0)
 	
